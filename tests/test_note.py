7d75192ffc6cbd63166136d389314515e584cb98
MaciejSzczesny <yetek.lol@gmail.com>
2020-12-07 15:52:49 +0100

    added test_note_init_creates_name

---
+++ tests/test_note.py
@@ -0,0 +1,21 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+from unittest import TestCase, main
+import assertpy
+from assertpy import assert_that
+
+from src.note.note import Note
+
+
+class TestNote(TestCase):
+    def setUp(self):
+        self.temp = Note()
+
+    def test_note_init__creates_name(self):
+        assert_that(self.temp).has_name("Andrzej")
+
+
+if __name__ == '__main__':
+    main()
