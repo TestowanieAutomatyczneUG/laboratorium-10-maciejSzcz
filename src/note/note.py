38723cec3e91a0bcfa48b6fbf892b383083b0aba
MaciejSzczesny <yetek.lol@gmail.com>
2020-12-07 16:03:00 +0100

    added an implementation for test_note_init_null_name_throws_exception

--- src/note/note.py
+++ src/note/note.py
@@ -2,3 +2,13 @@ class Note(object):
     def __init__(self, name, note):
         self.name = name
         self.note = note
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if value == None:
+            raise Exception("name can't be null")
+        self._name = value
